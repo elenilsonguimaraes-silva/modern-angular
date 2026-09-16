6d96d3829f0a4ccb53fbd3f3703e99b70dc87ff2
Elenilson G. Silva <videos.do.le@gmail.com>
2026-09-16 12:26:17 -0300

    update
    Update

---
+++ src/app/hello/hello.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  imports: [],
+  templateUrl: './hello.html',
+  styleUrl: './hello.scss',
+})
+export class Hello {
+
+  protected title= 'Welcome to  Modern Angular!';
+
+  protected isDisabled = false;
+
+  protected onClick() {
+    console.log(  'Button clicked!');
+    this.isDisabled = !this.isDisabled;
+
+}
+}
+
